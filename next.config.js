d9ec2a408ae4fbdb91bed0159fdfb2f206327d7a
Maxime Laharrague <maxime.laharrague@gmail.com>
2025-01-07 18:22:46 +0100

    deployment setup

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // This ensures static export works correctly
+  basePath: '',
+  // Disable server features we don't need for static export
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
